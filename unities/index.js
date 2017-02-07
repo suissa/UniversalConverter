d88b0fa1a0ab46ba537f9bf2bf9a12e7df40184b
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-02-07 17:49:43 -0300

    coisa feia

--- unities/index.js
+++ unities/index.js
@@ -71,4 +71,12 @@ module.exports = {
         name: 'Mililitro',
         'L': (val) => (val / 1000),
     },
+    'kg': {
+    name: 'Quilograma',
+    'g': (val) => (val  * 1000),
+  },
+    'g': {
+    name: 'Grama',
+    'kg': (val) => (val  / 1000),
+  },
 }
